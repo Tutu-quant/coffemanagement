37b1a0f285893e2049ab489f50a642c6dc3071ca
khanhly611 <dokhanhlly0611@gmail.com>
2026-07-11 01:14:37 +0700

    fix
    ...

--- CafeManagement/Models/User.cs
+++ CafeManagement/Models/User.cs
@@ -1,15 +1,19 @@
-﻿using System.Data;
-public class User
+﻿using System.ComponentModel.DataAnnotations;
+namespace CafeManagement.Models
 {
-    public int UserID { get; set; }
-    public int? EmployeeID { get; set; }
-    public int? CustomerID { get; set; }
-    public int RoleID { get; set; }
-    public string Username { get; set; }
-    public string PasswordHash { get; set; }
-    public DateTime? LastLogin { get; set; }
-    public DateTime? CreatedAt { get; set; }
-    public virtual Employee? Employee { get; set; }
-    public virtual Customer? Customer { get; set; }
-    public virtual Role Role { get; set; }
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        public int? EmployeeID { get; set; }
+        public int? CustomerID { get; set; }
+        public int RoleID { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime? LastLogin { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public virtual Employee? Employee { get; set; }
+        public virtual Customer? Customer { get; set; }
+        public virtual Role Role { get; set; } = null!;
+    }
 }
