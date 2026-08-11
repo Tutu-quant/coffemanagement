77cfcde1d2fccecdce7795058e1e99167419bb6b
Tutu-quant <namhoanglexnd@gmail.com>
2026-08-11 20:01:21 +0700

    update reservation time

---
+++ Migrations/20260810110839_AddReservationTime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quản_lý_quán_cafe.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReservationTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReservationTime",
+                table: "Reservations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReservationTime",
+                table: "Reservations");
+        }
+    }
+}
